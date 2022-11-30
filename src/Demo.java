e3665ad049206bcc530c06af6485959576630113
Requiem <public_receiver035@pm.me>
2022-11-30 16:59:25 +0700

    test

--- src/Demo.java
+++ src/Demo.java
@@ -2,19 +2,24 @@ import clinic.controller.*;
 
 public class Demo {
   public static void main(String[] args) throws Exception {
-    DoctorController doctorCtrl = new DoctorController();
-    NurseController nurseCtrl = new NurseController();
-    MedController medCtrl = new MedController();
-    PatientController patientCtrl = new PatientController();
+    // DoctorController doctorCtrl = new DoctorController();
+    // NurseController nurseCtrl = new NurseController();
+    // MedController medCtrl = new MedController();
+    // PatientController patientCtrl = new PatientController();
 
     // doctorCtrl.insertRecord("test", "test", "test", "test");
     // nurseCtrl.insertRecord("null", "null", "null", "null");
     // medCtrl.insertRecord("null", "null", 0, 0);
     // patientCtrl.insertRecord("null", "null", "null", "null", 0, false, "null");
 
-    doctorCtrl.deleteRecord("test");
-    nurseCtrl.deleteRecord("null");
-    medCtrl.deleteRecord("null");
-    patientCtrl.deleteRecord("null");
+    // doctorCtrl.deleteRecord("test");
+    // nurseCtrl.deleteRecord("null");
+    // medCtrl.deleteRecord("null");
+    // patientCtrl.deleteRecord("null");
+
+    // doctorCtrl.updateRecord("test", "null", "null", "null");
+    // nurseCtrl.updateRecord("null", "1", "1", "1");
+    // medCtrl.updateRecord("null", "1", 3.2, 120);
+    // patientCtrl.updateRecord("null", "1", "1", "1", 20, true, "1");
   }
 }
