fd0b68729d987be2ef4957efa8226e34c0adc03b
Requiem <public_receiver035@pm.me>
2022-12-06 21:39:43 +0700

    update

--- src/Demo.java
+++ src/Demo.java
@@ -17,43 +17,43 @@ public class Demo {
     clinic.view.MainPage gui = new clinic.view.MainPage();
     gui.setVisible(true);
   }
-  
-    // DoctorController doctorCtrl = new DoctorController();
-    // NurseController nurseCtrl = new NurseController();
-    // MedController medCtrl = new MedController();
-    // PatientController patientCtrl = new PatientController();
 
-    // clinic.view.doctor.Add addDoc = new clinic.view.doctor.Add();
-    // addDoc.setTitle("Add doctor");
-    // addDoc.setVisible(true);
+  // DoctorController doctorCtrl = new DoctorController();
+  // NurseController nurseCtrl = new NurseController();
+  // MedController medCtrl = new MedController();
+  // PatientController patientCtrl = new PatientController();
 
-    // clinic.view.nurse.Add addNur = new clinic.view.nurse.Add();
-    // addNur.setTitle("Add nurse");
-    // addNur.setVisible(true);
+  // clinic.view.doctor.Add addDoc = new clinic.view.doctor.Add();
+  // addDoc.setTitle("Add doctor");
+  // addDoc.setVisible(true);
 
-    // clinic.view.med.Add addMed = new clinic.view.med.Add();
-    // addMed.setTitle("Add medicine");
-    // addMed.setVisible(true);
+  // clinic.view.nurse.Add addNur = new clinic.view.nurse.Add();
+  // addNur.setTitle("Add nurse");
+  // addNur.setVisible(true);
 
-    // clinic.view.doctor.ShowList docList = new clinic.view.doctor.ShowList();
-    // docList.setTitle("Show doctor list");
-    // docList.setVisible(true);
+  // clinic.view.med.Add addMed = new clinic.view.med.Add();
+  // addMed.setTitle("Add medicine");
+  // addMed.setVisible(true);
 
-    // clinic.view.nurse.ShowList nurList = new clinic.view.nurse.ShowList();
-    // nurList.setTitle("Show nurse list");
-    // nurList.setVisible(true);
+  // clinic.view.doctor.ShowList docList = new clinic.view.doctor.ShowList();
+  // docList.setTitle("Show doctor list");
+  // docList.setVisible(true);
 
-    // clinic.view.med.ShowList medList = new clinic.view.med.ShowList();
-    // medList.setTitle("Show medicine list");
-    // medList.setVisible(true);
+  // clinic.view.nurse.ShowList nurList = new clinic.view.nurse.ShowList();
+  // nurList.setTitle("Show nurse list");
+  // nurList.setVisible(true);
 
-    // doctorCtrl.updateRecord("test", "null", "null", "null");
-    // nurseCtrl.updateRecord("null", "1", "1", "1");
-    // medCtrl.updateRecord("null", "1", 3.2, 120);
-    // patientCtrl.updateRecord("null", "1", "1", "1", 20, true, "1");
+  // clinic.view.med.ShowList medList = new clinic.view.med.ShowList();
+  // medList.setTitle("Show medicine list");
+  // medList.setVisible(true);
 
-    // doctorCtrl.showList();
-    // nurseCtrl.showList();
-    // medCtrl.showList();
-    // patientCtrl.showList();
+  // doctorCtrl.updateRecord("test", "null", "null", "null");
+  // nurseCtrl.updateRecord("null", "1", "1", "1");
+  // medCtrl.updateRecord("null", "1", 3.2, 120);
+  // patientCtrl.updateRecord("null", "1", "1", "1", 20, true, "1");
+
+  // doctorCtrl.showList();
+  // nurseCtrl.showList();
+  // medCtrl.showList();
+  // patientCtrl.showList();
 }
